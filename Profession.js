2701be5e9e61c82b67295178982fc0c000c9a992
kavindu9648 <keranga297@gmail.com>
2024-11-17 13:39:13 +0530

    Complete js part

---
+++ Profession.js
@@ -0,0 +1,37 @@
+//create add button
+document.getElementById("add").onclick = function () {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  const result = num1 + num2;
+  console.log(result);
+};
+//create sub button
+document.getElementById("sub").onclick = function () {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  if (num1 > num2) {
+    const result = num1 - num2;
+    console.log(result);
+  } else {
+    const result = num2 - num1;
+    console.log(result);
+  }
+};
+//create mul button
+document.getElementById("mul").onclick = function () {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  const result = num1 * num2;
+  console.log(result);
+};
+
+//create div button
+document.getElementById("div").onclick = function () {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  if (num1 > num2) {
+    const result = num1 / num2;
+  } else {
+    const result = num2 / num1;
+  }
+};

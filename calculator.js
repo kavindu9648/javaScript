c29a7f2fb92a11b56e6a054275279bfb89902511
kavindu9648 <keranga297@gmail.com>
2024-11-17 12:12:54 +0530

    Create Calculator

---
+++ calculator.js
@@ -0,0 +1,14 @@
+//This calculator Create by using Window prompt
+
+console.log("Calculator");
+
+//get User Inputs
+let firstNumber;
+firstNumber = window.prompt("Enter your First Number: ");
+console.log(firstNumber)
+
+let secondNumber;
+secondNumber = window.prompt("Enter your Second Number: ");
+console.log(secondNumber)
+
+
